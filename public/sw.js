19a1ce05c3914b41ec265eddc9b413aac8ec2b13
THEGYM <the_g@MacBook-Air.local>
2026-03-30 11:54:34 +0900

    feat: ポップアップを本日もお疲れ様でした！ボタンのみにシンプル化・SWv54

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'thegym-v52';
+const CACHE = 'thegym-v54';
 const ASSETS = ['/', '/index.html'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
